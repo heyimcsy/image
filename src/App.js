d7451eae48d9ac58abf4f4e3dcc3b446c3477a71
Seoyoon Choi <cinnamon1995@naver.com>
2023-03-17 18:09:18 +0900

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import photo from './image/hi.jpeg'
+import photo2 from './image/hi2.jpg'
+import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [files, setFiles] = useState('')
+
+  const addButton = (e) => {
+    const file = e.target.files
+    console.log(file)
+    setFiles(file)
+  }
+
+  const handleClick = (e) => {
+    const formdata = new FormData()
+    formdata.append('uploadImage', files[0])
+
+    const config = {
+      Headers: {
+        'content-type': 'multipart/form-data',
+      },
+    }
+    axios.post(`api`, formdata, config)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <img src={photo} alt="" />
+      <img src={photo2} />
+      <div>
+        <img src="" alt="" />
+        <input type="file" id="image" onChange={addButton} />
+        <label htmlFor="image">파일 선택하기</label>
+      </div>
+      <div>
+        <button onClick={handleClick}>저장하기</button>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
